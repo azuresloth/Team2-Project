5b99e78d43b038559045ed5e6bcbfd8878099705
kh202-15 <kh202-15@DESKTOP-BIJ2M5H>
2021-10-20 09:21:08 +0900

    item 기본 vo, mapper파일

---
+++ src/main/java/com/kh/project/item/vo/ImageVO.java
@@ -0,0 +1,43 @@
+package com.kh.project.item.vo;
+
+public class ImageVO {
+	private String imgCode;
+	private String originImgName;
+	private String attachedImgName;
+	private String itemCode;
+	private String isMain;
+	
+	
+	public String getImgCode() {
+		return imgCode;
+	}
+	public void setImgCode(String imgCode) {
+		this.imgCode = imgCode;
+	}
+	public String getOriginImgName() {
+		return originImgName;
+	}
+	public void setOriginImgName(String originImgName) {
+		this.originImgName = originImgName;
+	}
+	public String getAttachedImgName() {
+		return attachedImgName;
+	}
+	public void setAttachedImgName(String attachedImgName) {
+		this.attachedImgName = attachedImgName;
+	}
+	public String getItemCode() {
+		return itemCode;
+	}
+	public void setItemCode(String itemCode) {
+		this.itemCode = itemCode;
+	}
+	public String getIsMain() {
+		return isMain;
+	}
+	public void setIsMain(String isMain) {
+		this.isMain = isMain;
+	}
+	
+	
+}
--- src/main/java/com/kh/project/item/vo/ItemVO.java
+++ src/main/java/com/kh/project/item/vo/ItemVO.java
@@ -1,5 +1,64 @@
 package com.kh.project.item.vo;
 
-public class ItemVO {
+import java.util.List;
 
+public class ItemVO {
+	private String itemCode;
+	private String itemName;
+	private int itemPrice;
+	private String itemIntro;
+	private int itemStock;
+	private int itemStatus;
+	private String cateCode;
+	private List<ImageVO> imgList;
+	
+	public String getItemCode() {
+		return itemCode;
+	}
+	public void setItemCode(String itemCode) {
+		this.itemCode = itemCode;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public int getItemPrice() {
+		return itemPrice;
+	}
+	public void setItemPrice(int itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+	public String getItemIntro() {
+		return itemIntro;
+	}
+	public void setItemIntro(String itemIntro) {
+		this.itemIntro = itemIntro;
+	}
+	public int getItemStock() {
+		return itemStock;
+	}
+	public void setItemStock(int itemStock) {
+		this.itemStock = itemStock;
+	}
+	public int getItemStatus() {
+		return itemStatus;
+	}
+	public void setItemStatus(int itemStatus) {
+		this.itemStatus = itemStatus;
+	}
+	public String getCateCode() {
+		return cateCode;
+	}
+	public void setCateCode(String cateCode) {
+		this.cateCode = cateCode;
+	}
+	public List<ImageVO> getImgList() {
+		return imgList;
+	}
+	public void setImgList(List<ImageVO> imgList) {
+		this.imgList = imgList;
+	}
+	
 }
